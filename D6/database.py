e2b5fa91011683fe847b3930a4b37db5017eef62
Titrom25 <titrom25@mail.ru>
2022-06-01 13:04:37 +0700

    Add request 6

--- D6/database.py
+++ D6/database.py
@@ -1,14 +1,34 @@
 from flask import Flask, request, Response
 import psycopg2
+import datetime
 
 CURRENT_TIME = '2017-08-15 15:00:00.000000 +00:00'
 NEXT_DAY = '2017-08-16 15:00:00.000000 +00:00'
+
 
 class DB:
 	def __init__(self, host, database, user, password, port):
 		self.conn = psycopg2.connect(
 			host=host, database=database,
 			user=user, password=password, port=port)
+		
+	def _convertDate(self, textDate, next_day=False, twodays=False):
+		if textDate.find('.') > 0:
+			year, month, day = textDate.split('.')
+		elif textDate.find('-') > 0:
+			year, month, day = textDate.split('-')
+		else:
+			raise ValueError('Invalid date')
+
+		if next_day:
+			date_obj = datetime.datetime.strptime(f'{year}-{month}-{day}', '%Y-%m-%d').date()
+			skip_days = 2 if twodays else 1
+			date_obj += datetime.timedelta(days=skip_days)
+			year = str(date_obj.year)
+			month = str(date_obj.month).zfill(2)
+			day = str(date_obj.day).zfill(2)
+
+		return f'{year}-{month}-{day} 00:00:00.000000 +00:00'
 
 	def makeRequest(self, request):
 		cur = self.conn.cursor()
@@ -60,30 +80,65 @@ class DB:
 				airports.append((code, name["ru"], city['ru'], timezone))
 		return airports
 
-	def getInboundSchedule(self, airport_code):
+	def _getSchedule(self, airport_code, date, direction, twodays=False):
+		if date is None:
+			date_from = CURRENT_TIME
+			date_to = NEXT_DAY
+		else:
+			date_from = self._convertDate(date, twodays=twodays)
+			date_to = self._convertDate(date, next_day=True, twodays=twodays)
+
 		flights_data = self.makeRequest(f"""
 			SELECT t.*
 			FROM bookings.flights_v t
-			WHERE arrival_airport = '{airport_code}' and
-			scheduled_arrival > '{CURRENT_TIME}' and
-			scheduled_arrival < '{NEXT_DAY}'
-			ORDER BY scheduled_arrival
+			WHERE {direction}_airport = '{airport_code}' and
+			scheduled_{direction} >= '{date_from}' and
+			scheduled_{direction} <= '{date_to}'
+			ORDER BY scheduled_{direction}
 		""")
 		flights = []
 		for flight in flights_data:
-			flights.append((flight[1], flight[5], flight[9], flight[13]))
+			#					 dep_local  arr_local  dep_city   arr_city    flight_id  fligh_no   dep_glob  arr_glob    status      dep_airp   arr_airp    #price
+			flights.append((flight[3], flight[5], flight[9], flight[12], flight[0], flight[1], flight[2], flight[4], flight[13], flight[7], flight[10]))
 		return flights
 
-	def getOutboundSchedule(self, airport_code):
-		flights_data = self.makeRequest(f"""
-			SELECT t.*
-			FROM bookings.flights_v t
-			WHERE departure_airport = '{airport_code}' and
-			scheduled_departure > '{CURRENT_TIME}' and
-			scheduled_departure < '{NEXT_DAY}'
-			ORDER BY scheduled_departure
+	def getInboundSchedule(self, airport_code, date=None, twodays=False):
+		return self._getSchedule(airport_code, date, 'arrival', twodays)
+
+	def getOutboundSchedule(self, airport_code, date=None, twodays=False):
+		return self._getSchedule(airport_code, date, 'departure', twodays)
+
+	def getPricetable(self, flight_no):
+		pricetable = self.makeRequest(f"""
+			SELECT
+				string_agg(pricetb.seat_no, ', ' ORDER BY length(pricetb.seat_no), pricetb.seat_no),
+				pricetb.amount,
+				pricetb.fare_conditions
+			FROM (SELECT bp.seat_no,
+							tft.amount,
+							tft.fare_conditions
+					FROM (SELECT tf.flight_id,
+										tf.fare_conditions,
+										tf.amount,
+										tf.ticket_no
+							FROM ticket_flights tf) tft
+									INNER JOIN (SELECT ticket_no, flight_id, seat_no FROM boarding_passes) bp
+										ON bp.ticket_no = tft.ticket_no and bp.flight_id = tft.flight_id
+									LEFT JOIN (SELECT flight_id, flight_no FROM flights_v) f
+										ON f.flight_id = tft.flight_id
+									WHERE f.flight_no = '{flight_no}'
+					GROUP BY f.flight_no, bp.seat_no, tft.amount, tft.fare_conditions) as pricetb
+			GROUP BY pricetb.fare_conditions, amount
+			ORDER BY amount;
 		""")
-		flights = []
-		for flight in flights_data:
-			flights.append((flight[1], flight[3], flight[12], flight[13]))
-		return flights
+		prices = {}
+		for row in pricetable:
+			seat_class = row[2]
+			seat_price = row[1]
+			if seat_class in prices:
+				seat_class += '+'
+			prices[seat_class] = seat_price
+
+		print(flight_no, prices)
+		return prices
+
--- D6/server.py
+++ D6/server.py
@@ -1,6 +1,7 @@
 import enum
 from flask import Flask, request, Response
 import database
+import datetime
 
 app = Flask(__name__)
 
@@ -38,8 +39,9 @@ def getInboundSchedule():
 	airport = request.args.get("airport")
 	flights = db.getInboundSchedule(airport)
 	response_html = '<html>'
-	for (flight_no, arrival_time, departure_city, status) in flights:
-		response_html += f'<div>№ {flight_no}, Arrival time: {arrival_time}, From: {departure_city}, Status: {status}\n</div>'
+	for flight in flights:
+		_, arr_local, dep_city, _, _, flight_no, _, _, status, _, _ = flight
+		response_html += f'<div>№ {flight_no}, Arrival time: {arr_local}, From: {dep_city}, Status: {status}\n</div>'
 	return response_html + '</html>'
 
 @app.route('/getOutboundSchedule', methods=['GET'])
@@ -47,9 +49,104 @@ def getOutboundSchedule():
 	airport = request.args.get("airport")
 	flights = db.getOutboundSchedule(airport)
 	response_html = '<html>'
-	for (flight_no, arrival_time, departure_city, status) in flights:
-		response_html += f'<div>№ {flight_no}, Departure time: {arrival_time}, TO: {departure_city}, Status: {status}\n</div>'
+	for flight in flights:
+		dep_local, _, _, arr_city, _, flight_no, _, _, status, _, _ = flight
+		response_html += f'<div>№ {flight_no}, Departure time: {dep_local}, To: {arr_city}, Status: {status}\n</div>'
 	return response_html + '</html>'
 
+
+def _buildRoutes(source_airport, departure_airport, date, seat_class, limit):
+	response_html = '<html>'
+	ids2flight = {}
+	from_flights = {}
+
+	source_list = set([source_airport])
+	handled_surces = set()
+
+	for _ in range(int(limit) + 1):
+		for source in source_list.copy():
+			if source in handled_surces:
+				continue
+			handled_surces.add(source)
+
+			flights = db.getOutboundSchedule(source, date, twodays=True)
+			for flight in flights:  
+				dep_local, arr_local, dep_city, arr_city, flight_id, flight_no, dep_glob, arr_glob, status, dep_airp, arr_airp = flight
+
+				if (dep_airp not in from_flights):
+					from_flights[dep_airp] = []
+
+				if flight_id not in ids2flight:
+					from_flights[dep_airp].append(flight)
+					ids2flight[flight_id] = flight
+					# response_html += f'<div>№ {flight_no}, Departure time: {dep_glob}, Arrival time: {arr_glob}, From: {dep_city}, To: {arr_city}, Status: {status}\n</div>'
+					source_list.add(arr_airp)
+
+
+	routes = []
+	new_routes = []
+	correct_routes = []
+
+	for source in [source_airport]:
+		for flight in from_flights[source]:
+			routes.append([flight[4]])
+			
+	for _ in range(int(limit) + 1):
+		for route in routes:
+			last_flight = ids2flight[route[-1]]
+			last_flight_dest = last_flight[10]
+			if last_flight_dest == departure_airport:
+				correct_routes.append([ids2flight[route_flight] for route_flight in route])
+			else:
+				if last_flight_dest in from_flights:
+					for new_flight in from_flights[last_flight_dest]:
+						new_flight_dest = new_flight[3]
+						if new_flight_dest in [ids2flight[route_flight][2] for route_flight in route]:
+							continue
+						last_flight_arrival = last_flight[7]
+						new_flight_departure = new_flight[6]
+						if new_flight_departure > last_flight_arrival:
+							new_routes.append(route + [new_flight[4]])
+
+
+		routes = new_routes.copy()
+		new_routes = []
+
+
+	price_tables = {}
+	for route in correct_routes:
+		response_html += f'<h3>Route:</h3>'
+
+		total_price = 0
+		for flight in route:
+			dep_local, _, dep_city, arr_city, flight_id, flight_no, dep_glob, arr_glob, status, _, _ = flight
+			if flight_no in price_tables:
+				pricetb = price_tables[flight_no]
+			else:
+				pricetb = db.getPricetable(flight_no)
+				price_tables[flight_no] = pricetb
+
+			price = 15000
+			if seat_class in pricetb:
+				price = float(pricetb[seat_class])
+
+			total_price += price
+			response_html += f'<div>ID {flight_id}, № {flight_no}, Departure time: {dep_glob}, Arrival time: {arr_glob}, From: {dep_city}, To: {arr_city}, Price: {price}\n</div>'
+			
+		response_html += f'<h4>Total price: {total_price}, Total time: {route[-1][7] - route[0][6]}</h4>'
+	return response_html + '</html>'
+
+@app.route('/findRoutes', methods=['GET'])
+def findRoutes():
+	source = request.args.get("source")
+	destination = request.args.get("destination")
+	date = request.args.get("date")
+	seat_class = request.args.get("seat_class")
+	limit = request.args.get("limit")
+
+	routes = _buildRoutes(source, destination, date, seat_class, limit)
+
+	return routes
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+   app.run(host="0.0.0.0", port=5000)
